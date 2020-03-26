7828a26e6f84950c361b4f8c99938e9bd97a578e
amoodaa <amoodaa@gmail.com>
2020-03-26 14:22:10 +0200

    Added .vscode settings to enable eslint in 2 folders with different configs and added some utils for those who may use windows me git attributes to fix the linebreaks

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -13,7 +13,9 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018
   },
-  rules: {
+  rules: { 
+     // disables the windows/unix linebreak checks for anyone who will have to use windows
+    "linebreak-style": 0,
     "arrow-body-style": ["error", "as-needed"],
     // configure the prettier plugin
     "prettier/prettier": [
