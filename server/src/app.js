61a8744638e37f4e64b777d2626015765efa96f2
Lina Jamal <linahjamal89@gmail.com>
2020-03-28 22:35:50 +0300

    initalize express app,set port, listen the app at index.js, install morgan

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { join } = require('path');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+
+const router = require('./router');
+
+const app = express();
+
+app.disabled('x-powered-by');
+
+app.set('port', process.env.PORT || 5000);
+
+const middlewares = [
+  compression(),
+  cookieParser(),
+  express.json(),
+  express.urlencoded({ extended: false }),
+];
+
+app.use(middlewares);
+
+app.use(express.static(join(__dirname, '..', '..', 'client', 'public')));
+app.use(router);
+module.exports = app;
