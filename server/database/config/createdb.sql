9fe452275032c1d7993ccee33e8765983fe14ca6
Lina Ebeid <linaebe0@gmail.com>
2020-03-29 10:44:08 +0300

    structue modified

--- server/database/config/createdb.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE roombooker;
-CREATE USER booker WITH superuser password '123abc';
-ALTER DATABASE roombooker OWNER TO booker;
--- test db
-CREATE DATABASE roombtest;
-CREATE USER bookertest WITH superuser password '123cde';
-ALTER DATABASE roombtest OWNER TO bookertest;
