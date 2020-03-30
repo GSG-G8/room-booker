8eb518a5d8226e1e96dda940967dfee6b4cb5bff
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-03-30 10:06:56 +0300

    /index.html route relates #37

--- server/src/app.js
+++ server/src/app.js
@@ -22,6 +22,7 @@ app.use(middlewares);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(join(__dirname, '..', '..', 'client', 'build')));
+  app.get('*', (req, res) => res.sendFile(join(__dirname, '/index.html')));
 }
 app.use(router);
 
