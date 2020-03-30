5e1cdb7662b2ff9cc5891c2e79a3d4ea61985e6e
amoodaa <amoodaa@gmail.com>
2020-03-30 17:11:37 +0300

    added console.error for when a server error happens

--- server/src/controllers/error.js
+++ server/src/controllers/error.js
@@ -1,7 +1,8 @@
 exports.clientError = (req, res) => {
   res.status(404).send({ message: 'Page Not Found', statusCode: 404 });
 };
-// eslint-disable-next-line no-unused-vars
-exports.serverError = (err, req, res, next) => {
+exports.serverError = (err, req, res) => {
+  // eslint-disable-next-line no-console
+  if (process.env.NODE_ENV !== 'development') console.error(err);
   res.status(500).send({ message: 'Internal Server Error', statusCode: 500 });
 };
