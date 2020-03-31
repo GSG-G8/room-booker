33215fe13fea544288ec70b068409de5339d4dde
Lina Ebeid <linaebe0@gmail.com>
2020-03-31 22:58:57 +0300

    remove console

--- server/src/database/config/connection.js
+++ server/src/database/config/connection.js
@@ -6,7 +6,6 @@ let dbUrl = '';
 
 if (process.env.NODE_ENV === 'test') {
   dbUrl = process.env.TEST_DB_URL;
-  console.log(process.env)
 } else if (process.env.NODE_ENV === 'production') {
   dbUrl = process.env.DATABASE_URL;
 } else {
