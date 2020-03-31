00879d55ba8757fdbdb0d0040fea10e3f72e198b
Imad Shatali <amoodaa@gmail.com>
2020-03-31 20:48:42 +0300

    hoohoo

--- server/src/database/config/connection.js
+++ server/src/database/config/connection.js
@@ -6,18 +6,33 @@ let dbUrl = '';
 
 if (process.env.NODE_ENV === 'test') {
   dbUrl = process.env.TEST_DB_URL;
-  console.log(process.env)
 } else if (process.env.NODE_ENV === 'production') {
   dbUrl = process.env.DATABASE_URL;
 } else {
   dbUrl = process.env.DB_URL;
 }
 
-if (!dbUrl) throw new Error('No Database FOUND');
+// eslint-disable-next-line no-console
+console.log({
+  dbUrl,
+  POSTGRES_PORT: process.env.POSTGRES_PORT,
+  POSTGRES_HOST: process.env.POSTGRES_HOST,
+});
 
-const options = {
-  connectionString: dbUrl,
-  ssl: true,
-};
+if (!(dbUrl || process.env.POSTGRES_PORT || process.env.POSTGRES_HOST))
+  throw new Error('No Database FOUND');
+
+const options = dbUrl
+  ? {
+      connectionString: dbUrl,
+      ssl: true,
+    }
+  : {
+      user: 'postgres',
+      database: 'postgres',
+      password: 'postgres',
+      port: process.env.POSTGRES_PORT,
+      host: process.env.POSTGRES_HOST,
+    };
 
 module.exports = new Pool(options);
