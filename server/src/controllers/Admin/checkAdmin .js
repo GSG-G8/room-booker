632d477b5e8ce5e3111e10a187a3402e9d55143a
Lina Jamal <linahjamal89@gmail.com>
2020-04-01 22:22:58 +0300

    add checkadmin middleware before delete user

--- server/src/controllers/Admin/checkAdmin .js
+++ server/src/controllers/Admin/checkAdmin .js
@@ -1,8 +1,8 @@
 const Boom = require('@hapi/boom');
 
 module.exports = (req, res, next) => {
-  console.log(req.user, '99', req.is_admin);
-  if (req.user && req.user.is_admin) {
+  console.log('99', req.is_admin);
+  if (req.user.is_admin) {
     next();
   } else {
     throw Boom.unauthorized('You are not admin');
