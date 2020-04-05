d2e3b88afdf7dd44c2158799801f7c123340d3d5
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-04-05 14:54:55 +0300

    old password validation relates #16

--- server/__test__/patchProfile.test.js
+++ server/__test__/patchProfile.test.js
@@ -17,6 +17,7 @@ test('GET /patchProfile, logged in user valid request ', (done) => {
     ])
     .send({
       name: 'Imad2',
+      oldPassword: '123456',
       password: '12345678',
     })
     .expect('Content-Type', /json/)
@@ -38,6 +39,7 @@ test('GET /patchProfile, logged in user invalid password', (done) => {
     ])
     .send({
       name: 'Imad2',
+      oldPassword: '123456',
       password: 'qwe',
     })
     .expect('Content-Type', /json/)
@@ -57,7 +59,29 @@ test('GET /patchProfile, logged in user, name is required', (done) => {
       'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjIsInJvbGUiOnRydWUsImlhdCI6MTU4NTc1Njk3OH0.KslFtCQbzhtakHYZU_q1Pid-QuLlEMRwJ3dCWKG4lDk',
     ])
     .send({
+      oldPassword: '123456',
       password: '12345678',
+    })
+    .expect('Content-Type', /json/)
+    .expect(400)
+    .end((err) => {
+      if (err) return done(err);
+      return done();
+    });
+});
+
+test('GET /patchProfile, user does not exists', (done) => {
+  request(app)
+    .patch('/api/v1/patchProfile')
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json')
+    .set('Cookie', [
+      'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOi0xLCJyb2xlIjpmYWxzZSwiaWF0IjoxNTg2MDg2MzAyfQ.tPi_QBtUkUlpbg0nXH94bwkOaDZCOj6WLRCZLyTMK8Q',
+    ])
+    .send({
+      name: 'Imad2',
+      oldPassword: '123456',
+      password: '123456',
     })
     .expect('Content-Type', /json/)
     .expect(400)
@@ -74,6 +98,7 @@ test('GET /patchProfile, Unauthorized user', (done) => {
     .set('Accept', 'application/json')
     .send({
       name: 'Imad2',
+      oldPassword: '123456',
       password: '12345678',
     })
     .expect('Content-Type', /json/)
--- server/src/controllers/patchProfile.js
+++ server/src/controllers/patchProfile.js
@@ -2,13 +2,29 @@ const Boom = require('@hapi/boom');
 const bcrypt = require('bcrypt');
 const patchProfile = require('../database/queries/patchProfile');
 const profileScema = require('./validation/profileSchema');
+const getUserById = require('../database/queries/getUserById');
 
 module.exports = (req, res, next) => {
-  const { name, password } = req.body;
+  const { userID } = req.user;
+  const { name, oldPassword = '', password } = req.body;
+
   profileScema
     .validateAsync({ name, password }, { abortEarly: false })
-    .then(() => bcrypt.hash(req.body.password, 10))
-    .then((hash) => patchProfile(req.user.userID, name, hash))
+    .then(() => getUserById(userID))
+    .then((users) => {
+      if (users.rows.length === 0) {
+        throw new Error('user does not exists');
+      }
+      return users.rows[0].password;
+    })
+    .then((hashed) => bcrypt.compare(oldPassword, hashed))
+    .then((match) => {
+      if (!match) {
+        throw new Error('old password is wrong');
+      }
+    })
+    .then(() => bcrypt.hash(password, 10))
+    .then((hash) => patchProfile(userID, name, hash))
     .then((results) => res.send(results.rows.length !== 0))
     .catch((error) => {
       next(Boom.badRequest(error));
