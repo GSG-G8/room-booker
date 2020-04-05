408dbbf6b36a9cd129dfffc2411aa26f34009740
AbeerKaram <abeerkhalid571@gmail.com>
2020-04-05 12:27:16 +0300

    response end when status 404 #15

--- server/src/controllers/getProfile.js
+++ server/src/controllers/getProfile.js
@@ -3,7 +3,7 @@ const { getUserById } = require('../database/queries');
 module.exports = (req, res) => {
   getUserById(req.user.userID).then((users) => {
     if (users.rows.length === 0) {
-      res.status(404).json();
+      res.status(404).end();
     } else res.json(users.rows[0]);
   });
 };
