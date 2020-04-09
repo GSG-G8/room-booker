a62fb1717dcf2dbf9125f6b71595b0f74edcc505
Lina Jamal <linahjamal89@gmail.com>
2020-04-09 19:33:26 +0300

    full calendar

--- client/src/components/DayView/Calendar.js
+++ client/src/components/DayView/Calendar.js
@@ -1,8 +1,7 @@
 // import interactionPlugin from '@fullcalendar/interaction'; // needed for dayClick
-// import timeGridPlugin from '@fullcalendar/timegrid';
-
-import dayGridPlugin from '@fullcalendar/daygrid';
+// import dayGridPlugin from '@fullcalendar/daygrid';
 import FullCalendar from '@fullcalendar/react';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import React from 'react';
 import './style.css';
 
@@ -10,12 +9,20 @@ import './style.css';
 // render() {
 const Calendar = () => (
   <FullCalendar
-    defaultView="dayGrid"
-    plugins={[dayGridPlugin]}
+    defaultView="timeGridWeek"
+    plugins={[timeGridPlugin]}
+    header={{
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+    }}
+    editable="true"
+    resourceLabelText="Rooms"
     events={[
       { title: 'Meeting 1', date: '2020-04-08' },
       { title: 'Meeting 2', date: '2020-04-11' },
     ]}
+    timeZone="UTC"
   />
 );
 export default Calendar;
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import Calendar from '../../components/DayView/Calendar';
 import './style.css';
 
-const Home = () => <div className="Home" />;
+const Home = () => (
+  <div className="Home">
+    <Calendar />
+  </div>
+);
 
 export default Home;
