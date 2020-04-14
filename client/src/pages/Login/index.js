d6b914e29b7303ae158c245fc9adcb406c428e64
AbeerKaram <abeerkhalid571@gmail.com>
2020-04-14 14:03:06 +0300

    Use antd Form and Form.Item #7

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,8 +1,8 @@
-import { Button, Input } from 'antd';
+import { Button, Form, Input } from 'antd';
+import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import loginImg from '../../assets/loginImg.png';
-import logInSchema from '../../utils/loginValidation';
 import './style.css';
 
 class Login extends React.Component {
@@ -15,18 +15,20 @@ class Login extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     const { email, password } = this.state;
-    return logInSchema
-      .isValid({ email, password })
-      .then(() =>
-        fetch('/api/v1/login', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            email,
-            password,
-          }),
-        })
-      )
+    fetch('/api/v1/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((result) => {
+        const {
+          history: { push },
+        } = this.props;
+        push(`/profile${result.data.message}`);
+      })
       .then((res) => {
         if (!res) {
           this.setState({
@@ -48,40 +50,41 @@ class Login extends React.Component {
     return (
       <div className="login">
         <img src={loginImg} alt="loginImage" className="login__image" />
-        <p className="login__title">Welcome to parent assistent system</p>
-        <form className="login__form" onSubmit={this.handleSubmit}>
-          <p className="login__header">LOGIN</p>
-
-          <Input
-            label="Email"
-            name="email"
-            value={email}
-            type="email"
-            placeholder="Email"
-            className="login__input"
-            onChange={({ target }) => this.setState({ email: target.value })}
-          />
-
-          <Input
-            label="Password"
-            name="password"
-            value={password}
-            type="password"
-            placeholder="Passwrod"
-            className="login__input"
-            onChange={({ target }) => this.setState({ password: target.value })}
-          />
+        <p className="login__header">LOGIN</p>
+        <Form className="login__form" onSubmit={this.handleSubmit}>
+          <Form.Item className="login__input">
+            <Input.Email
+              placeholder="Email"
+              value={email}
+              onChange={({ target }) => this.setState({ email: target.value })}
+            />
+            <Input.Password
+              placeholder="Password"
+              value={password}
+              onChange={({ target }) =>
+                this.setState({ password: target.value })
+              }
+            />
+          </Form.Item>
           <div>
-            <Button className="login__button" value="LogIn" />
+            <Button className="login__button" value="LogIn">
+              LOGIN
+            </Button>
           </div>
-          <Link className="link" to="/forgetPassword">
-            Forget password?
-          </Link>
-          {errorMessage}
-        </form>
+        </Form>
+
+        <Link className="link" to="/forgetPassword">
+          Forget password?
+        </Link>
+        {errorMessage}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+  push: PropTypes.func.isRequired,
+};
 
 export default Login;
