48273afeee8883f11d23b82f9ae91de6e8e1d99f
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-04-16 10:27:58 +0300

    dashboard page

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,6 +1,48 @@
+// import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Layout } from 'antd';
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Menu2 from '../../components/Menu';
+import Rooms from '../../components/Table/Rooms';
+import Users from '../../components/Table/Users';
 import './style.css';
 
-const Dashboard = () => <div className="Dashboard" />;
+const { Content, Sider } = Layout;
+
+function Dashboard() {
+  return (
+    <Router>
+      <Layout>
+        <Sider width={200} className="layout-background">
+          <Menu2 />
+        </Sider>
+        <Layout>
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            <Switch>
+              <Route path="/dashboard/admin">
+                <h2>admin</h2>
+              </Route>
+              <Route path="/dashboard/users">
+                <Users />
+              </Route>
+              <Route path="/dashboard/rooms">
+                <Rooms />
+              </Route>
+              <Route path="/">
+                <h2>dashboard</h2>
+              </Route>
+            </Switch>
+          </Content>
+        </Layout>
+      </Layout>
+    </Router>
+  );
+}
 
 export default Dashboard;
