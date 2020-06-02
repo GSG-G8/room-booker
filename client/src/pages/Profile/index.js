a38eab5821808f0f76af62fa3718be00b9e65791
Lina Jamal <linahjamal89@gmail.com>
2020-06-02 23:13:32 +0300

    fetch profile data and make form to use data

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import { LockOutlined, MailOutlined, EditOutlined } from '@ant-design/icons';
 // eslint-disable-next-line no-unused-vars
-import { notification, Spin } from 'antd';
+import { Button, Form, Input, notification, Spin } from 'antd';
 import './style.css';
 
 class Profile extends React.Component {
   state = {
     // loading: true,
-    // error: false,
     profileData: {},
   };
 
@@ -32,9 +32,50 @@ class Profile extends React.Component {
 
   render() {
     const { profileData } = this.state;
+    const { name, email } = profileData;
     // eslint-disable-next-line no-console
     console.log(profileData);
-    return <div className="Profile" />;
+    return (
+      <div className="profile">
+        <Form className="profile__form">
+          <Form.Item name="name" label="Name" className="profile__input">
+            <Input disabled placeholder={name} prefix={<EditOutlined />} />
+          </Form.Item>
+          <Form.Item name="email" label="Email" className="profile__input">
+            <Input disabled placeholder={email} prefix={<MailOutlined />} />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            className="profile__input"
+          >
+            <Input
+              disabled
+              placeholder="*********"
+              type="password"
+              prefix={<LockOutlined />}
+            />
+          </Form.Item>
+          <div className="profile__button">
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="profile__button--update"
+            >
+              update profile
+            </Button>
+            <Button
+              disabled
+              type="primary"
+              htmlType="submit"
+              className="profile__button--save"
+            >
+              save
+            </Button>
+          </div>
+        </Form>
+      </div>
+    );
   }
 }
 export default Profile;
