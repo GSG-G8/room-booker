361ba0a82e343b04e7c916859b5fec2a7f017dd7
Lina Jamal <linahjamal89@gmail.com>
2020-06-02 14:18:22 +0300

    fetch profile date

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,6 +1,40 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import { notification, Spin } from 'antd';
 import './style.css';
 
-const Profile = () => <div className="Profile" />;
+class Profile extends React.Component {
+  state = {
+    // loading: true,
+    // error: false,
+    profileData: {},
+  };
 
+  componentDidMount() {
+    this.fetchProfileData();
+  }
+
+  fetchProfileData = () =>
+    fetch(`/api/v1/profile`)
+      .then((res) => {
+        if (!res.ok) {
+          res.json().then(({ message: msg }) => notification.error(msg));
+          throw res.statusText;
+        }
+        return res.json();
+      })
+      .then((results) => {
+        this.setState({ profileData: results });
+      })
+      .catch((err) => {
+        notification.error(err);
+      });
+
+  render() {
+    const { profileData } = this.state;
+    // eslint-disable-next-line no-console
+    console.log(profileData);
+    return <div className="Profile" />;
+  }
+}
 export default Profile;
