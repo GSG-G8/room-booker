7f5b1ac91ad02412b26fbe97556d9cdf432d8eb6
Lina Jamal <linahjamal89@gmail.com>
2020-06-03 00:41:38 +0300

    add navbar to profile page

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { LockOutlined, MailOutlined, EditOutlined } from '@ant-design/icons';
 // eslint-disable-next-line no-unused-vars
 import { Button, Form, Input, notification, Spin } from 'antd';
+import Navbar from '../../components/Navbar';
+
 import './style.css';
 
 class Profile extends React.Component {
@@ -36,53 +38,57 @@ class Profile extends React.Component {
     // eslint-disable-next-line no-console
     console.log(profileData);
     return (
-      <div className="profile">
-        <h2 className="profile__header">
-          <span className="profile__header__apan">Profile </span> Page
-        </h2>
-        <Form
-          className="profile__form"
-          labelCol={{
-            span: 6,
-          }}
-          labelAlign="left"
-        >
-          <Form.Item name="name" label="Name" className="profile__input">
-            <Input disabled placeholder={name} prefix={<EditOutlined />} />
-          </Form.Item>
-          <Form.Item name="email" label="Email" className="profile__input">
-            <Input disabled placeholder={email} prefix={<MailOutlined />} />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            label="Password"
-            className="profile__input"
+      <div>
+        <Navbar />
+
+        <div className="profile">
+          <h2 className="profile__header">
+            <span className="profile__header__apan">Profile </span> Page
+          </h2>
+          <Form
+            className="profile__form"
+            labelCol={{
+              span: 6,
+            }}
+            labelAlign="left"
           >
-            <Input
-              disabled
-              placeholder="*********"
-              type="password"
-              prefix={<LockOutlined />}
-            />
-          </Form.Item>
-          <div className="profile__button">
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="profile__button--update"
+            <Form.Item name="name" label="Name" className="profile__input">
+              <Input disabled placeholder={name} prefix={<EditOutlined />} />
+            </Form.Item>
+            <Form.Item name="email" label="Email" className="profile__input">
+              <Input disabled placeholder={email} prefix={<MailOutlined />} />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              label="Password"
+              className="profile__input"
             >
-              update profile
-            </Button>
-            <Button
-              disabled
-              type="primary"
-              htmlType="submit"
-              className="profile__button--save"
-            >
-              save
-            </Button>
-          </div>
-        </Form>
+              <Input
+                disabled
+                placeholder="*********"
+                type="password"
+                prefix={<LockOutlined />}
+              />
+            </Form.Item>
+            <div className="profile__button">
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="profile__button--update"
+              >
+                update profile
+              </Button>
+              <Button
+                disabled
+                type="primary"
+                htmlType="submit"
+                className="profile__button--save"
+              >
+                save
+              </Button>
+            </div>
+          </Form>
+        </div>
       </div>
     );
   }
