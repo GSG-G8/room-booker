d420d7daf406b6c6daba03bfc50f7fcf499b1012
Lina Jamal <linahjamal89@gmail.com>
2020-06-03 22:27:34 +0300

    make cancle button , add initialValues and ref

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { LockOutlined, MailOutlined, EditOutlined } from '@ant-design/icons';
-// eslint-disable-next-line no-unused-vars
 import { Button, Form, Input, notification, Spin } from 'antd';
 import Navbar from '../../components/Navbar';
 
@@ -13,13 +12,15 @@ class Profile extends React.Component {
     isUpdate: false,
   };
 
+  formRef = React.createRef();
+
   componentDidMount() {
     this.fetchProfileData().then(() => this.setState({ loading: false }));
   }
 
-  nameOnChange = (e) => {
-    this.setState({ profileData: { name: e.target.value } });
-  };
+  // nameOnChange = (e) => {
+  //   this.setState({ profileData: { name: e.target.value } });
+  // };
 
   toggleUpdate = () => {
     const { isUpdate } = this.state;
@@ -30,12 +31,13 @@ class Profile extends React.Component {
     fetch(`/api/v1/profile`)
       .then((res) => {
         if (!res.ok) {
-          res.json().then(({ message: msg }) => notification.error(msg));
+          res.json().then(notification.error);
           throw res.statusText;
         }
         return res.json();
       })
       .then((results) => {
+        // this.formRef.current.setFieldsValue(results);
         this.setState({ profileData: results });
       })
       .catch((err) => {
@@ -53,16 +55,24 @@ class Profile extends React.Component {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ name, oldPassword, password }),
-    }).then((res) => {
-      if (!res.ok) {
-        notification.error('could not update ');
-      } else {
-        notification.success({
-          message: 'data has been updated',
-        });
-        this.setState({ isUpdate: !isUpdate });
-      }
-    });
+    })
+      .then((res) => {
+        if (!res.ok) {
+          res.json().then(notification.error);
+        } else {
+          notification.success({
+            message: 'data has been updated',
+          });
+          this.setState({ isUpdate: !isUpdate });
+          this.formRef.current.setFieldsValue({
+            oldPassword: '',
+            password: '',
+          });
+        }
+      })
+      .catch((err) => {
+        notification.error(err);
+      });
   };
 
   render() {
@@ -85,22 +95,29 @@ class Profile extends React.Component {
               span: 7,
             }}
             labelAlign="left"
+            ref={this.formRef}
             initialValues={{
               name,
               email,
+              // password: '',
+              // oldPassword: '',
             }}
             onFinish={(values) => this.updateProfile(values)}
           >
             <Form.Item name="name" label="Name" className="profile__input">
               <Input
                 disabled={!isUpdate}
-                placeholder={name}
+                // placeholder={name}
                 prefix={<EditOutlined />}
                 onChange={this.nameOnChange}
               />
             </Form.Item>
             <Form.Item name="email" label="Email" className="profile__input">
-              <Input disabled placeholder={email} prefix={<MailOutlined />} />
+              <Input
+                disabled
+                // placeholder={email}
+                prefix={<MailOutlined />}
+              />
             </Form.Item>
             {isUpdate && (
               <Form.Item
@@ -108,39 +125,49 @@ class Profile extends React.Component {
                 label="oldPassword"
                 className="profile__input"
               >
+                <Input type="password" prefix={<LockOutlined />} />
+              </Form.Item>
+            )}
+            {isUpdate && (
+              <Form.Item
+                name="password"
+                label="Password"
+                className="profile__input"
+              >
                 <Input
-                  placeholder="*********"
+                  disabled={!isUpdate}
                   type="password"
                   prefix={<LockOutlined />}
                 />
               </Form.Item>
             )}
-            <Form.Item
-              name="password"
-              label="Password"
-              className="profile__input"
-            >
-              <Input
-                disabled={!isUpdate}
-                placeholder="*********"
-                type="password"
-                prefix={<LockOutlined />}
-              />
-            </Form.Item>
-
             <div className="profile__button">
-              <Button
-                onClick={this.toggleUpdate}
-                disabled={isUpdate}
-                type="primary"
-                // htmlType="submit"
-                className="profile__button--update"
-              >
-                update profile
-              </Button>
+              {isUpdate || (
+                <Button
+                  onClick={this.toggleUpdate}
+                  type="primary"
+                  // htmlType="submit"
+                  className="profile__button--update"
+                >
+                  update profile
+                </Button>
+              )}
               {isUpdate && (
                 <Button
-                  disabled={!isUpdate}
+                  // disabled={!isUpdate}
+                  type="primary"
+                  // htmlType="submit"
+                  className="profile__button--save"
+                  onClick={() => {
+                    this.setState({ isUpdate: false });
+                  }}
+                >
+                  cancel
+                </Button>
+              )}
+              {isUpdate && (
+                <Button
+                  // disabled={!isUpdate}
                   type="primary"
                   htmlType="submit"
                   className="profile__button--save"
